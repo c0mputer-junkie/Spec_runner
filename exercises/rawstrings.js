7d32f3f7f114e2ef730d117530d77050d575ec6d
Your Name <your_email@whatever.com>
2016-04-10 12:18:59 -0700

    pass second test

--- exercises/rawstrings.js
+++ exercises/rawstrings.js
@@ -11,9 +11,8 @@ Feel free to use any string methods you wish.
 
 // Gives back ("returns") the fourth letter in a string
 var printLetter1 = function () { 
-  
-console.log("this is working!")
-  return'print the fourth letter in this string'[3];
+  console.log('this is working')
+  return'print the fourth letter in this string';
 };
 
 /*
@@ -24,5 +23,5 @@ var bridgeString1 = function(myInputString) {
   
   'I wish I could code  in javascript all day, every day!';
 
-  return "concatenated"'I wish I could code  in javascript all day, every day!';
+  return 'I wish I could code  in javascript all day, every day!';
 };
